62390e278721e72118696857ba9872c510b353b3
reynn211 <144656505+reynn211@users.noreply.github.com>
2024-04-28 00:54:36 +0300

    login page 2, fix templates

--- movies/settings.py
+++ movies/settings.py
@@ -70,7 +70,9 @@ ROOT_URLCONF = "movies.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, 'static')
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
