59635a42520cd5d3f714fe5bb25b770756cbc00e
reynn211 <144656505+reynn211@users.noreply.github.com>
2024-05-25 01:03:17 +0300

    minor fix

--- movies/settings.py
+++ movies/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [os.getenv('HOST_URL'),'localhost', '127.0.0.1']
 
